48e98fef853e567177987fc3f16f613d43aab354
Lillia <luizkadu20@gmail.com>
2025-06-19 21:03:39 -0300

    arrays e secoes

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 const buques = [
-    
+    {
+        nome:"Raio de sol",
+        preco: 139.99,
+        imagem: "imagens/buquegirassoisgipsofila.jpg"
+    },
+    {
+        nome:"Sol de Outono",
+        preco: 149.99,
+        imagem:"imagens/buquegirassoisrosabranca.jpg"
+    },
+    {
+        nome:"Amor Intenso",
+        preco: 119.99,
+        imagem:"imagens/buquerosa.jpg" 
+    },
+    {
+        nome:"Brisa de Champagne",
+        preco:119.99,
+        imagem:"imagens/buquerosasbrancas.jpg"
+    }
 ]
+const arranjos = [
+    {
+        nome:"Aurora do Campo",
+        preco:69.90,
+        imagem:"imagens/arranjozinias.jpg"
+    },
+    {
+        nome:"Vintage Rosé",
+        preco:79.90,
+        imagem:"imagens/arranjocrisantemo.jpg"
+    },
+    {
+        nome:"Harmonia Pura",
+        preco:49.90,
+        imagem:"imagens/arranjoorquideas.jpg"
+    },
+    {
+        
+    }
+]
+
